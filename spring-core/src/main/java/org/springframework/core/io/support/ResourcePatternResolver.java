4d8757116c4aa28dca1b376a6a45ad6e31432269
chenzhenyuan@skyable.cn <1253665625@qq.com>
2020-08-30 17:21:13 +0800

    ResourcePatternResolver

--- spring-core/src/main/java/org/springframework/core/io/support/ResourcePatternResolver.java
+++ spring-core/src/main/java/org/springframework/core/io/support/ResourcePatternResolver.java
@@ -51,6 +51,23 @@ import org.springframework.core.io.ResourceLoader;
  * @see org.springframework.context.ApplicationContext
  * @see org.springframework.context.ResourceLoaderAware
  */
+
+/**
+ * 策略接口，用于将位置模式（例如 Ant风格的路径模式）解析为Resource对象。
+ * 这是{@link org.springframework.core.io.ResourceLoader}接口的扩展。
+ * 可以检查传入的ResourceLoader（例如，在上下文中运行时通过{@link org.springframework.context.ResourceLoaderAware}传入的{{link org.springframework.context.ApplicationContext}）
+ * 是否可以实现这个扩展接口。
+ * {@link PathMatchingResourcePatternResolver}是一个独立的实现可以在ApplicationContext外部使用，
+ * 也可以由{@link ResourceArrayPropertyEditor}用于填充资源数组bean属性。
+ * 可与任何类型的位置模式一起使用（例如“ /WEB-INF/*-context.xml”）：输入模式必须与策略实现相匹配。该接口仅指定转换方法，而不是特定的模式格式。
+ * 此接口还为来自类路径的所有匹配资源建议一个新的资源前缀“ classpath *：”。
+ * 请注意，在这种情况下，资源位置应该是不带占位符的路径（例如“ /beans.xml”）；
+ * JAR文件或类目录可以包含多个相同名称的文件。
+ * @see org.springframework.core.io.Resource
+ * @see org.springframework.core.io.ResourceLoader
+ * @see org.springframework.context.ApplicationContext
+ * @see org .springframework.context.ResourceLoaderAware
+ */
 public interface ResourcePatternResolver extends ResourceLoader {
 
 	/**
@@ -59,6 +76,11 @@ public interface ResourcePatternResolver extends ResourceLoader {
 	 * retrieves all matching resources for a given name (e.g. "/beans.xml"),
 	 * for example in the root of all deployed JAR files.
 	 * @see org.springframework.core.io.ResourceLoader#CLASSPATH_URL_PREFIX
+	 */
+	/**
+	 * 类路径中所有匹配资源的伪URL前缀：“ classpath *：”
+	 * 这与ResourceLoader的类路径URL前缀不同，因为它检索给定名称（例如“ /beans.xml”）的所有匹配资源，例如在所有已部署的JAR文件的根目录中。
+	 * @see org.springframework.core.io.ResourceLoader＃CLASSPATH_URL_PREFIX
 	 */
 	String CLASSPATH_ALL_URL_PREFIX = "classpath*:";
 
@@ -70,6 +92,12 @@ public interface ResourcePatternResolver extends ResourceLoader {
 	 * @param locationPattern the location pattern to resolve
 	 * @return the corresponding Resource objects
 	 * @throws IOException in case of I/O errors
+	 */
+	/**
+	 * 给定的位置模式解析为Resource对象。
+	 * 应尽可能避免指向相同物理资源的重叠资源条目。结果应具有设定的语义。
+	 * @param locationPattern 对位置模式进行解析
+	 * @return 相应的Resource对象
 	 */
 	Resource[] getResources(String locationPattern) throws IOException;
 
