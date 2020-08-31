e2b70dbc7f9b8ddd098ebeab88c7628d00dfbd3e
chenzhenyuan@skyable.cn <1253665625@qq.com>
2020-08-31 23:19:43 +0800

    AbstractRefreshableWebApplicationContext

--- spring-web/src/main/java/org/springframework/web/context/support/AbstractRefreshableWebApplicationContext.java
+++ spring-web/src/main/java/org/springframework/web/context/support/AbstractRefreshableWebApplicationContext.java
@@ -78,10 +78,30 @@ import org.springframework.web.context.ServletContextAware;
  * @see org.springframework.ui.context.ThemeSource
  * @see XmlWebApplicationContext
  */
+
+/**
+ * {@link org.springframework.context.support.AbstractRefreshableApplicationContext}子类，
+ * 该子类为Web环境实现了{{link org.springframework.web.context.ConfigurableWebApplicationContext}接口。提供一个“configLocations”属性，在Web应用程序启动时通过ConfigurableWebApplicationContext接口填充。
+ * 此类与AbstractRefreshableApplicationContext一样容易子类化：您只需实现{@link #loadBeanDefinitions}方法即可；
+ * 注意，应该假定实现是通过{@link #getConfigLocations}方法返回的位置指定的文件中加载bean定义的。
+ * 将资源路径解释为Servlet上下文资源，即，作为Web应用程序根目录下的路径。绝对路径，例如对于Web应用程序根目录之外的文件，可以通过{@link org.springframework.core.io.DefaultResourceLoader}实现的“文件：” URL访问*。
+ * 除了{{link org.springframework.context.support.AbstractApplicationContext}检测到的特殊bean外，此类还检测{@link org.springframework.ui.context.ThemeSource}类型的bean在上下文中，使用特殊的Bean名称“themeSource”。
+ * 这是要为另一种bean定义格式而子类化的Web上下文。对于{@link org.springframework.web.context.ContextLoader}，可以将这样的上下文实现指定为“contextClass”context-param * 或作为“contextClass”
+ * {@link org.springframework.web.servlet.FrameworkServlet}的初始化参数，替换默认的{@link XmlWebApplicationContext}。然后，它将自动分别接收“contextConfigLocation”上下文参数或init参数。
+ * 请注意，通常应假定WebApplicationContext实现是根据通过{@link ConfigurableWebApplicationContext}接口接收的配置进行自身配置的。
+ * 相反，独立的应用程序上下文可能允许使用自定义启动代码进行配置（例如{@link org.springframework.context.support.GenericApplicationContext}）。
+ * @see #loadBeanDefinitions
+ * @see org.springframework.web.context.ConfigurableWebApplicationContext＃setConfigLocations
+ * @see org.springframework.ui.context.ThemeSource
+ * @see XmlWebApplicationContext
+ */
 public abstract class AbstractRefreshableWebApplicationContext extends AbstractRefreshableConfigApplicationContext
 		implements ConfigurableWebApplicationContext, ThemeSource {
 
 	/** Servlet context that this context runs in. */
+	/**
+	 * 这个容器上下文运行的{@link ServletContext}
+	 */
 	@Nullable
 	private ServletContext servletContext;
 
@@ -164,12 +184,18 @@ public abstract class AbstractRefreshableWebApplicationContext extends AbstractR
 	/**
 	 * Register request/session scopes, a {@link ServletContextAwareProcessor}, etc.
 	 */
+	/**
+	 * {@link org.springframework.beans.factory.BeanFactory}的后处理
+	 * 注册request/session范围，{@link ServletContextAwareProcessor}等。
+	 * 可以处理{@link ServletContextAware}
+	 * @param beanFactory 应用程序上下文使用的bean工厂
+	 */
 	@Override
 	protected void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {
 		beanFactory.addBeanPostProcessor(new ServletContextAwareProcessor(this.servletContext, this.servletConfig));
 		beanFactory.ignoreDependencyInterface(ServletContextAware.class);
 		beanFactory.ignoreDependencyInterface(ServletConfigAware.class);
-
+		/* 注册request/session的scope */
 		WebApplicationContextUtils.registerWebApplicationScopes(beanFactory, this.servletContext);
 		WebApplicationContextUtils.registerEnvironmentBeans(beanFactory, this.servletContext, this.servletConfig);
 	}
@@ -177,6 +203,11 @@ public abstract class AbstractRefreshableWebApplicationContext extends AbstractR
 	/**
 	 * This implementation supports file paths beneath the root of the ServletContext.
 	 * @see ServletContextResource
+	 */
+	/**
+	 * 此实现支持ServletContext根目录下的文件路径。 * @see ServletContextResource
+	 * @param path the path to the resource
+	 * @return
 	 */
 	@Override
 	protected Resource getResourceByPath(String path) {
