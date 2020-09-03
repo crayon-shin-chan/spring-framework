fd50bc46844f97d13bf5c98f16fc6c446cb8072a
chenzhenyuan@skyable.cn <1253665625@qq.com>
2020-09-03 18:54:58 +0800

    GenericBeanDefinition

--- spring-beans/src/main/java/org/springframework/beans/factory/support/GenericBeanDefinition.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/GenericBeanDefinition.java
@@ -37,6 +37,17 @@ import org.springframework.util.ObjectUtils;
  * @see RootBeanDefinition
  * @see ChildBeanDefinition
  */
+
+/**
+ * GenericBeanDefinition是一站式商店，用于标准bean定义。
+ * 与任何bean定义一样，它允许指定一个类以及可选的构造函数参数值和属性值。
+ * 另外，可以通过“parentName”属性灵活地配置从父bean定义派生的内容。
+ * 通常，使用此{@code GenericBeanDefinition}类用于注册用户可见的bean定义（后处理器可能在其上进行操作，甚至可能重新配置父名称）。
+ * 使用{@code RootBeanDefinition}{@code ChildBeanDefinition}，其中父/子关系恰好是预先确定的。
+ * @see #setParentName
+ * @see RootBeanDefinition
+ * @see ChildBeanDefinition
+ */
 @SuppressWarnings("serial")
 public class GenericBeanDefinition extends AbstractBeanDefinition {
 
