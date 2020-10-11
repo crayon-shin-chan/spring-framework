a4f19e16e67cd065519c38e4ee79a36bbcc49c14
chenzhenyuan@skyable.cn <1253665625@qq.com>
2020-10-11 22:55:28 +0800

    ContextAnnotationAutowireCandidateResolver

--- spring-context/src/main/java/org/springframework/context/annotation/ContextAnnotationAutowireCandidateResolver.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ContextAnnotationAutowireCandidateResolver.java
@@ -46,15 +46,31 @@ import org.springframework.util.Assert;
  * @author Juergen Hoeller
  * @since 4.0
  */
+
+/**
+ *  {@link org.springframework.beans.factory.support.AutowireCandidateResolver}策略接口的完整实现，为限定符注释以及延迟解析提供支持，这些注释由{@link Lazy}注释驱动context.annotation。
+ */
 public class ContextAnnotationAutowireCandidateResolver extends QualifierAnnotationAutowireCandidateResolver {
 
+	/**
+	 * 获取懒加载解析代理
+	 * @param descriptor
+	 * @param beanName
+	 * @return
+	 */
 	@Override
 	@Nullable
 	public Object getLazyResolutionProxyIfNecessary(DependencyDescriptor descriptor, @Nullable String beanName) {
 		return (isLazy(descriptor) ? buildLazyResolutionProxy(descriptor, beanName) : null);
 	}
 
+	/**
+	 * 指定依赖是否需要懒加载
+	 * @param descriptor：依赖描述符
+	 * @return
+	 */
 	protected boolean isLazy(DependencyDescriptor descriptor) {
+		/** 判断{@link Lazy}注解 */
 		for (Annotation ann : descriptor.getAnnotations()) {
 			Lazy lazy = AnnotationUtils.getAnnotation(ann, Lazy.class);
 			if (lazy != null && lazy.value()) {
@@ -62,6 +78,7 @@ public class ContextAnnotationAutowireCandidateResolver extends QualifierAnnotat
 			}
 		}
 		MethodParameter methodParam = descriptor.getMethodParameter();
+		/** 方法参数存在，方法为null或者返回值为void */
 		if (methodParam != null) {
 			Method method = methodParam.getMethod();
 			if (method == null || void.class == method.getReturnType()) {
@@ -74,13 +91,21 @@ public class ContextAnnotationAutowireCandidateResolver extends QualifierAnnotat
 		return false;
 	}
 
+	/**
+	 * 构建懒加载解析代理
+	 * @param descriptor
+	 * @param beanName
+	 * @return
+	 */
 	protected Object buildLazyResolutionProxy(final DependencyDescriptor descriptor, final @Nullable String beanName) {
 		BeanFactory beanFactory = getBeanFactory();
 		Assert.state(beanFactory instanceof DefaultListableBeanFactory,
 				"BeanFactory needs to be a DefaultListableBeanFactory");
 		final DefaultListableBeanFactory dlbf = (DefaultListableBeanFactory) beanFactory;
 
+		/** 被代理目标源 */
 		TargetSource ts = new TargetSource() {
+			/* 目标类型 */
 			@Override
 			public Class<?> getTargetClass() {
 				return descriptor.getDependencyType();
@@ -89,10 +114,14 @@ public class ContextAnnotationAutowireCandidateResolver extends QualifierAnnotat
 			public boolean isStatic() {
 				return false;
 			}
+			/* 被代理对象 */
 			@Override
 			public Object getTarget() {
+				/* 被注入bean名称 */
 				Set<String> autowiredBeanNames = (beanName != null ? new LinkedHashSet<>(1) : null);
+				/* 解析依赖返回目标对象 */
 				Object target = dlbf.doResolveDependency(descriptor, beanName, autowiredBeanNames, null);
+				/* 如果没有解析到依赖，集合类型返回默认值 */
 				if (target == null) {
 					Class<?> type = getTargetClass();
 					if (Map.class == type) {
@@ -107,6 +136,7 @@ public class ContextAnnotationAutowireCandidateResolver extends QualifierAnnotat
 					throw new NoSuchBeanDefinitionException(descriptor.getResolvableType(),
 							"Optional dependency not present for lazy injection point");
 				}
+				/* 存在bean名称，注册bean名称依赖 */
 				if (autowiredBeanNames != null) {
 					for (String autowiredBeanName : autowiredBeanNames) {
 						if (dlbf.containsBean(autowiredBeanName)) {
@@ -120,13 +150,15 @@ public class ContextAnnotationAutowireCandidateResolver extends QualifierAnnotat
 			public void releaseTarget(Object target) {
 			}
 		};
-
+		/* 代理工厂 */
 		ProxyFactory pf = new ProxyFactory();
 		pf.setTargetSource(ts);
 		Class<?> dependencyType = descriptor.getDependencyType();
+		/* 如果是接口 */
 		if (dependencyType.isInterface()) {
 			pf.addInterface(dependencyType);
 		}
+		/* 创建代理 */
 		return pf.getProxy(dlbf.getBeanClassLoader());
 	}
 
